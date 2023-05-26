3efaf1ba067c66d8fd78d4d6df10fb6e198f4a3f
Jeremy Boloh <boloh.jeremy@gmail.com>
2023-05-26 10:30:12 +0200

    create-router + Navlink

--- kasa/src/App.js
+++ kasa/src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Accommodation from "./pages/Accommodation";
+import Error from "./pages/Error";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/accommodation" element={<Accommodation />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
